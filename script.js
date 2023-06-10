5b9ab8f3abda070acb7c4fab29758ffd5abb421d
LUYUE0507 <ly13120581281@163.com>
2023-06-10 17:02:00 +0800

    upload

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const fileInputs = document.querySelectorAll('input[type="file"]');
-
-fileInputs.forEach(input => {
-	input.addEventListener('change', () => {
-		const file = input.files[0];
-		if (file) {
-			const fileName = file.name;
-			input.previousSibling.textContent = fileName;
-			input.parentNode.setAttribute('data-file', fileName);
-		}
-	});
-});
+function greetUser() {
+	var name = document.getElementById("nameInput").value;
+	alert("很高兴认识你，" + name + "!");
+}
