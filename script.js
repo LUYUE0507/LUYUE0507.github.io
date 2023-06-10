0e2e030d9af95ed05e8c25cf052816ba631dc173
LUYUE0507 <ly13120581281@163.com>
2023-06-10 17:50:46 +0800

    upload

--- script.js
+++ script.js
@@ -2,14 +2,3 @@ function greetUser() {
 	var name = document.getElementById("nameInput").value;
 	alert("很高兴认识你，" + name + "!");
 }
-
-$(document).ready(function() {
-  $('.navbar-nav a').click(function(e) {
-    e.preventDefault();
-    var section = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(section).offset().top
-    }, 1000);
-  });
-});
-```
